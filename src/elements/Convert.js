8b7f5eb58b678e03439af65e4ed224d1023f08b1
Xavi <xavier.stoa@gmail.com>
2024-02-07 14:56:36 +0100

    Added fully functional clear button

--- src/elements/Convert.js
+++ src/elements/Convert.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ButtonElement from './ButtonElement';
 
-function Convert({ ctexte, updateCtexte}) {
+function Convert({ ctexte, updateCtexte }) {
 
     function copier() {
         navigator.clipboard.writeText(ctexte.current.value);
     }
 
+    const [textAreaValue, setTextAreaValue] = useState('');
+
+    const handleTextAreaChange = (event) => {
+        setTextAreaValue(event.target.value);
+    };
+
+    const ntext = useRef();
+
+    const clearTextArea = () => {
+        setTextAreaValue('');
+        ntext.current.focus()
+    };
+
     return (
         <div className="border-2 rounded my-2 flex">
             <div className='m-4 flex-grow flex justify-center flex-col md:flex-row gap-3'>
                 <div className='w-full flex flex-col'>
+                <ButtonElement
+                        arrowFunction={clearTextArea}
+                        text='Clear'
+                        bStyle='md:hidden block place-self-end' />
                     <textarea
+                        ref={ntext}
                         name="texte"
+                        value={textAreaValue}
+                        onChange={handleTextAreaChange}
                         id="texte"
                         className='p-4 w-full flex-grow border border-gray-300 rounded-md resize-none'
                         placeholder='Saisir votre message à crypter/décrypter'
                     />
                 </div>
-                <div className='flex md:flex-col flex-row justify-around gap-5'>
+                <div className='flex md:flex-col flex-row justify-center md:justify-around gap-5'>
+                    <ButtonElement
+                        arrowFunction={clearTextArea}
+                        text='Clear'
+                        bStyle='hidden md:block place-self-start' />
                     <ButtonElement
                         arrowFunction={() => updateCtexte(1)}
                         bStyle='bg-secondary rounded'
                         extra={{}}
                         pStyle='m-8'
                         text='Crypter' />
-                    <textarea className='cursor-text w-full resize-none hidden'/>
                     <ButtonElement
                         arrowFunction={() => updateCtexte(0)}
                         bStyle='bg-secondary rounded'
